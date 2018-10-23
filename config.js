96b51d74abe649b353e7310072414a1a65a29bcc
nsanpedro <nicolassanpedro@gmail.com>
2018-10-23 17:27:13 -0300

    1st commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: '4N6jCpQgQJMalR03sW93e539G',
+    consumer_secret: 'Ahprt2RIDnGXmFe05VHeGXAD78EUuMPczUIs7xctc2PbJZn4kl',
+    access_token_key: '1054822743252918272-HUif3B11PrPL23EJzsIbJdOlgAOJlT',
+    access_token_secret: 'VHHA218VomdBZXz44iP2dyEz8SYgCSWodqCZFretzGkPq'
+};
